38783bfc0df70f5cfc1b0bd4c2e2ad5ae73ddf74
David Moruzzi <dmoruzzi@users.noreply.github.com>
2024-07-28 23:13:27 -0400

    ISO 8601 Support

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,81 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const epochInput = document.getElementById("epochInput");
-    const localOutput = document.getElementById("localOutput");
-    const utcOutput = document.getElementById("utcOutput");
-    const dateInput = document.getElementById("dateInput");
-    const timeInput = document.getElementById("timeInput");
-    const epochOutput = document.getElementById("epochOutput");
+    // Elements
+    const elements = {
+        epochInput: document.getElementById("epochInput"),
+        localOutput: document.getElementById("localOutput"),
+        utcOutput: document.getElementById("utcOutput"),
+        dateInput: document.getElementById("dateInput"),
+        timeInput: document.getElementById("timeInput"),
+        epochOutput: document.getElementById("epochOutput"),
+        isoInput: document.getElementById("isoInput"),
+        convertISOToLocalBtn: document.getElementById("convertISOToLocalBtn"),
+        convertEpochToLocalBtn: document.getElementById("convertEpochToLocalBtn"),
+        convertMsToLocalBtn: document.getElementById("convertMsToLocalBtn"),
+        convertLocalToEpochBtn: document.getElementById("convertLocalToEpochBtn"),
+        quickOptions: document.querySelectorAll(".quick-option")
+    };
 
+    // Event Listeners
+    elements.convertEpochToLocalBtn.addEventListener("click", convertEpochToLocal);
+    elements.convertMsToLocalBtn.addEventListener("click", msToEpoch);
+    elements.convertLocalToEpochBtn.addEventListener("click", convertLocalToEpoch);
+    elements.convertISOToLocalBtn.addEventListener("click", convertISOToEpoch);
+    elements.quickOptions.forEach(option => option.addEventListener("click", () => setQuickOption(parseInt(option.dataset.hours))));
+
+    // Initialize
+    setQuickOption(0);
+
+    // Functions
     function msToEpoch() {
-        const msValue = parseInt(epochInput.value);
-        if (isNaN(msValue)) {
-            localOutput.textContent = "Conversion Error: Invalid time provided";
-            utcOutput.textContent = "";
-            return;
-        }
-        const epochTime = Math.floor(msValue / 1000);
-        epochInput.value = epochTime;
+        const msValue = parseInt(elements.epochInput.value, 10);
+        if (isNaN(msValue)) return showError(elements.localOutput, "Invalid time provided");
+        elements.epochInput.value = Math.floor(msValue / 1000);
         convertEpochToLocal();
     }
 
     function convertEpochToLocal() {
-        const epochValue = parseInt(epochInput.value);
-        if (isNaN(epochValue)) {
-            localOutput.textContent = "Conversion Error: Invalid time provided";
-            utcOutput.textContent = "";
-            return;
-        }
-        const date = new Date(epochValue * 1000);
-        const localTime = formatDate(date);
-        const utcTime = formatDate(date, true);
+        const epochValue = parseInt(elements.epochInput.value, 10);
+        if (isNaN(epochValue)) return showError(elements.localOutput, "Invalid time provided");
 
-        displayTime(localOutput, "Local Time: ", localTime);
-        displayTime(utcOutput, "UTC Time: ", utcTime);
+        const date = new Date(epochValue * 1000);
+        updateOutputs(date);
+        elements.isoInput.value = formatISO(date);
+        convertISOToEpoch();
+    }
+
+    function convertLocalToEpoch() {
+        const localInput = `${elements.dateInput.value}T${elements.timeInput.value}${getTimezoneOffsetString()}`;
+        const epochTime = Date.parse(localInput) / 1000;
+
+        if (isNaN(epochTime)) return showError(elements.epochOutput, "Invalid date or time format");
+        elements.isoInput.value = localInput;
+        convertISOToEpoch();
+    }
+
+    function convertISOToEpoch() {
+        const isoString = elements.isoInput.value;
+        const date = new Date(isoString);
+
+        if (isNaN(date.getTime())) return showError(elements.localOutput, "Invalid ISO 8601 string");
+
+        const epochTime = Math.floor(date.getTime() / 1000);
+        updateOutputs(date);
+        elements.epochOutput.textContent = `Epoch Time: ${epochTime}`;
+
+        // Update Local Date and Time inputs
+        elements.dateInput.value = formatDateToString(date);
+        elements.timeInput.value = formatTime(date);
+        elements.epochInput.value = epochTime;
+    }
+
+    function updateOutputs(date) {
+        elements.localOutput.textContent = `Local Time: ${formatDate(date)}`;
+        elements.utcOutput.textContent = `UTC Time: ${formatDate(date, true)}`;
+    }
+
+    function showError(element, message) {
+        element.textContent = `Conversion Error: ${message}`;
+        elements.utcOutput.textContent = "";
     }
 
     function formatDate(date, isUTC = false) {
@@ -46,30 +91,20 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    function convertLocalToEpoch() {
-        const localInput = dateInput.value + "T" + timeInput.value + getTimezoneOffsetString();
-        const epochTime = Date.parse(localInput) / 1000;
-
-        if (isNaN(epochTime)) {
-            epochOutput.textContent = "Conversion Error: Invalid date or time format";
-        } else {
-            displayTime(epochOutput, "Epoch Time: ", epochTime);
-        }
+    function formatISO(date) {
+        return date.toISOString().split('Z')[0] + formatTimezoneOffset();
     }
 
-    function displayTime(element, label, time) {
-        element.textContent = label + time;
+    function formatTime(date) {
+        return date.toTimeString().split(' ')[0];
     }
 
     function setQuickOption(hours) {
         const date = new Date();
         date.setHours(date.getHours() - hours);
 
-        const formattedTime = date.toLocaleTimeString([], { hour12: false });
-        const formattedDate = formatDateToString(date);
-        
-        dateInput.value = formattedDate;
-        timeInput.value = formattedTime;
+        elements.dateInput.value = formatDateToString(date);
+        elements.timeInput.value = formatTime(date);
         convertLocalToEpoch();
     }
 
@@ -86,23 +121,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function getTimezoneOffsetString() {
         const offsetMinutes = new Date().getTimezoneOffset();
-        const offsetHours = offsetMinutes / 60;
-        const sign = offsetHours <= 0 ? '+' : '-';
-        const absOffsetHours = Math.abs(offsetHours);
-        const formattedOffset = `${sign}${pad(absOffsetHours)}:${pad(offsetMinutes % 60)}`;
-        return formattedOffset;
+        const sign = offsetMinutes <= 0 ? '+' : '-';
+        const hours = Math.abs(Math.floor(offsetMinutes / 60));
+        const minutes = Math.abs(offsetMinutes % 60);
+        return `${sign}${pad(hours)}:${pad(minutes)}`;
     }
 
-    document.getElementById("convertEpochToLocalBtn").addEventListener("click", convertEpochToLocal);
-    document.getElementById("convertMsToLocalBtn").addEventListener("click", msToEpoch);
-    document.getElementById("convertLocalToEpochBtn").addEventListener("click", convertLocalToEpoch);
-
-    const quickOptions = document.querySelectorAll(".quick-option");
-    quickOptions.forEach(option => {
-        option.addEventListener("click", function () {
-            setQuickOption(parseInt(option.dataset.hours));
-        });
-    });
-
-    setQuickOption(0);
+    function formatTimezoneOffset() {
+        return getTimezoneOffsetString().replace(/:/g, '');
+    }
 });
